5da73bab134317547e741d4b4ae01031df5d82f5
cahalls3 <carsonahalls3@gmail.com>
2022-02-28 09:37:27 -0700

    yuh

--- Mission8/Controllers/HomeController.cs
+++ Mission8/Controllers/HomeController.cs
@@ -1,10 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mission8.Models;
+using Mission8.Models.ViewModels;
+
 namespace Mission8.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public HomeController()
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
         {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
         }
     }
 }
--- Mission8/Infrastructure/PaginationTagHelper.cs
+++ Mission8/Infrastructure/PaginationTagHelper.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission8.Models.ViewModels;
+
 namespace Mission8.Infrastructure
 {
-    public class PaginationTagHelper
+    [HtmlTargetElement("div", Attributes = "page-money")]
+    public class PaginationTagHelper : TagHelper
     {
-        public PaginationTagHelper()
+        //Dynamically create the page links for us
+
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper(IUrlHelperFactory temp)
         {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        //Different than the View Context
+        public PageInfo PageMoney { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i < PageMoney.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
         }
     }
 }
